4df1d6aa12748af9aa04b5247e4984112bc47362
Bruno Rafael <brunorafaeldls2016@gmail.com>
2022-10-29 21:10:20 -0300

    clear code

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -1,87 +1,15 @@
-// const userService = require('../services/UserService');
+const userService = require('../services/UserService');
 const router = require('express').Router();
 
-router.get('/', async (req, res) => {
-  
-  try {
+router.get('/', userService.getAll);
 
-    const users = await User.find();
-    res.status(200).json(users);
-    
-  } catch (error) {
+router.get('/:id', userService.getById);
 
-    res.status(500).json(error);
-  
-  }
-});
+router.post('/', userService.create);
 
-router.get('/:id', async (req, res) => {
-  
-  try {
+router.patch('/:id', userService.update);
 
-    const id = req.params.id;
-    const user = await User.findOne({_id: id});
-
-    res.status(200).json(user);
-    
-  } catch (error) {
-
-    res.status(500).json(error);
-  
-  }
-});
-
-router.post('/', async (req, res) => {
-
-  const {nome, cpf, telefone} = req.body;
-
-  const user = {nome, cpf, telefone};
-
-  try {
-    
-    await User.create(user);
-    res.status(201).json({ message: "User created!" })
-
-  } catch (error) {
-
-    res.status(500).json(error);
-  
-  }
-
-});
-
-router.patch('/:id', async (req, res) => {
-
-  const id = req.params.id;
-
-  const {nome, cpf, telefone} = req.body;
-
-  const user = {nome, cpf, telefone};
-
-  try {
-
-    await User.updateOne({_id: id}, user);
-    res.status(200).json(user);
-    
-  } catch (error) {
-    res.status(500).json(error);
-  }
-});
-
-router.delete('/:id', async (req, res) => {
-
-  const id= req.params.id;
-
-  try {
-
-    await User.deleteOne({_id: id});
-    res.status(200).json({message: "usuário deletado"});
-    
-  } catch (error) {
-    res.status(500).json(error);
-  }
-
-});
+router.delete('/:id', userService.deleteById);
 
 
 module.exports = router;
--- services/UserService.js
+++ services/UserService.js
@@ -1,17 +1,59 @@
-// const User = require('../models/UserModel');
+const User = require('../models/UserModel');
 
-// const getAll = 
+exports.getAll = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+}
 
-// const getById = 
+exports.getById = async (req, res) => {
+  try {
+    const user = await User.findOne({_id: req.params.id});
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+}
 
-// const create = 
+exports.create = async (req, res) => {
+  try {
+    await User.create({
+      nome: req.body.nome,
+      cpf: req.body.cpf,
+      telefone: req.body.telefone,
+    });
+    res.status(201).json({ message: "User created!" })
+  } catch (error) {
+    res.status(500).json(error);
+  }
 
-// const update = 
+}
 
-// const deleteById = 
+exports.update = async (req, res) => {
+  try {
+    await User.updateOne({_id: req.params.id},
+       {
+        nome: req.body.nome,
+        cpf: req.body.cpf,
+        telefone: req.body.telefone,
+       }
+    );
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+}
 
-// module.exports = getById;
-// module.exports = getAll;
-// module.exports = create;
-// module.exports = update;
-// module.exports = deleteById;
+exports.deleteById = async (req, res) => {
+  try {
+    await User.deleteOne({_id: req.params.id});
+    res.status(200).json({message: "usuário deletado"});
+  } catch (error) {
+    res.status(500).json(error);
+  }
+
+}
+
